8d21dd84e4602d2a26721abf40c21dd99d80dfff
Michelangelo Cali <michelangelocali@hotmail.com>
2022-06-16 20:56:44 -0300

    projeto final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Login from "./components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./components/User/User";
+import ProtectedRoute from "./components/Helper/ProtectedRoute";
+import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
+import NotFound from "./components/Helper/NotFound";
 
 function App() {
-  return <div>App React</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
